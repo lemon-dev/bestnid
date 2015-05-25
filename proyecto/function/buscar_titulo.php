4b4cd98be5389b1ce53d878124bf6bbbe6362687
santi-git <santi.figueiras95@gmail.com>
2015-05-24 22:01:23 -0300

    Problemas tratando de revertir cambios, vuelvo a subir

--- proyecto/function/buscar_titulo.php
+++ proyecto/function/buscar_titulo.php
@@ -1,8 +1,18 @@
-<?php
-	//TODO: Función de búsqueda por título
-	include("../include/header.php");
+<?php 
+include("../include/header.php"); 
+include("../db/connect.php");
+?>
 
+<?php 
+	$query = " 	SELECT *
+				FROM Subasta
+				WHERE Titulo LIKE '%" . $_GET["param_busqueda"] . "%'
+	";
+	$result = $db->query($query); 
+	while ($row = $result->fetch_object()) {
+		echo $row->titulo . '<br>' . $row->descripcion, '<br>';
+	} 
+?>
 	
+<?php include("../include/footer.php"); ?>
 
-	include("../include/footer.php");
- ?>
