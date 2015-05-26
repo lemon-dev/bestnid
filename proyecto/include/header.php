1c443e0f3bfb6d2a669042193ffd34177bfb9f38
Juan Cruz Almazán <juancrujca@gmail.com>
2015-05-26 16:43:24 -0300

    gui header
    interfaz grafica del header

--- proyecto/include/header.php
+++ proyecto/include/header.php
@@ -17,17 +17,38 @@
 	<link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-	<nav>
-		<ul>
-			<li><img src="#" alt="logo"></li>
-			<li><a href="#">Bestnid</a></li>
-			<li>
-				<form method="get" action="/function/buscar_titulo.php">
-					<input type="text" placeholder="Search" value="" name="param_busqueda">
-					<button type="submit">Buscar</button>
+	<!-- Navigation -->
+	<nav class="navbar navbar-fixed-top" role="navigation">
+		<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand page-scroll" href="#page-top">
+					<div>
+						<img class="logo" src="img/logo/bestnid.png" alt="logo"> lemon-dev
+					</div>
+				</a>
+			</div>
+	
+			<!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Ingresar</a></li>
+					<li><a href="/register.php">Registrarse</a></li>
+				</ul>
+				<form class="navbar-form navbar-right" role="search" method="get" action="/function/buscar_titulo.php">
+					<div class="form-group">
+						<input type="text" placeholder="Busque aquí..." value="" name="param_busqueda" class="form-control">
+					</div>
+					<button type="submit" class="btn btn-default">Buscar</button>
 				</form>
-			</li>	
-			<li><a href="/register.php">Registrarse</a></li>
-			<li><a href="#">Loguearse</a></li>
-		</ul>
+			</div>
+			<!-- /.navbar-collapse -->
+		</div>
+		<!-- /.container -->
 	</nav>
