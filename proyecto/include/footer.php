e1d06fa292bbe8d73047348107654fade1ff8a75
Juan Cruz Almazán <juancrujca@gmail.com>
2015-06-01 17:18:14 -0300

    footer y main.js

--- proyecto/include/footer.php
+++ proyecto/include/footer.php
@@ -19,5 +19,6 @@
 	</footer>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script type="text/javascript" src="/js/materialize.min.js"></script>
+  <script type="text/javascript" src="/js/main.js"></script>
 </body>
 </html>
