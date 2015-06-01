08742b1ace5fc606953ad7cbc79df8f1cd245da6
Juan Cruz Almazán <juancrujca@gmail.com>
2015-06-01 17:05:11 -0300

    footer

--- proyecto/include/footer.php
+++ proyecto/include/footer.php
@@ -1,12 +1,21 @@
 <?php //El footer se modifica solo de acá ?>
 
 	<footer class="page-footer red darken-1">
+		<div class="container">
+            <div class="row">
+				<div class="col-md-0">	
+					<a class="grey-text text-lighten-4 left" href="../termsycond">términos y condiciones</a>
+					<a class="grey-text text-lighten-4 right" href="https://github.com/lemon-dev"><img class="responsive-img"src="../img/github.png" alt="Logo"> </a>
+				</div>
+            </div>
+          </div>
           <div class="footer-copyright">
             <div class="container">
             © 2015 lemon-dev ALL RIGHTS RESERVED
             <a class="grey-text text-lighten-4 right" href="http://lemon-dev.esy.es">lemon-dev.esy.es</a>
             </div>
           </div>
+		</div>
 	</footer>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script type="text/javascript" src="/js/materialize.min.js"></script>
