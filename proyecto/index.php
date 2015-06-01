eb60cc7d9caf8bea98c919402da05691e565e947
santi-git <santi.figueiras95@gmail.com>
2015-06-01 19:08:15 -0300

    Comenzando a hacer la ubicacion del ordenar

--- proyecto/index.php
+++ proyecto/index.php
@@ -8,6 +8,19 @@
 	$pageTitle = "Bestnid";
 	include("include/header.php"); 
 ?>
+
+<nav class="red darken-1">
+  <ul class="right hide-on-med-and-down">
+    <li><a href="#!">First Sidebar Link</a></li>
+    <li><a href="#!">Second Sidebar Link</a></li>
+  </ul>
+  <ul id="slide-out" class="side-nav">
+    <li><a href="#!">First Sidebar Link</a></li>
+    <li><a href="#!">Second Sidebar Link</a></li>
+  </ul>
+  <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+</nav>
+
 <section>
 	<div class="container">
 		<div class="container">
