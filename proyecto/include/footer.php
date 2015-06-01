28413fd322314a88d5bd5583062f69603c004344
Juan Cruz Almazán <juancrujca@gmail.com>
2015-06-01 15:44:33 -0300

    footer
    mejora del footer

--- proyecto/include/footer.php
+++ proyecto/include/footer.php
@@ -3,30 +3,23 @@
 	<footer class="page-footer red darken-1">
 		<div class="container">
             <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">Footer Content</h5>
-                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
-                <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                </ul>
-              </div>
+				<div class="col 10 s13">
+					<h5 class="white-text">Bestnid</h5>
+				</div>
+				<div class="col 13 s10">
+					<p class="grey-text text-lighten-4">Un sitio web para subastas distinto, en el cuál lo que más importa no es quien más da, sino quién más necesita.</p>
+				</div>
             </div>
           </div>
           <div class="footer-copyright">
             <div class="container">
-            © 2015 lemon-dev
+            © 2015 lemon-dev ALL RIGHTS RESERVED
             <a class="grey-text text-lighten-4 right" href="http://lemon-dev.esy.es">lemon-dev.esy.es</a>
+			<a href="https://github.com/lemon-dev"><i class="small fa fa-github"></i></a>
             </div>
           </div>
 	</footer>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script type="text/javascript" src="/js/materialize.min.js"></script>
-  <script type="text/javascript" src="/js/main.js"></script>
 </body>
 </html>
