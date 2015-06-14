1fca670a0d8131130b488678bc1aa308b0ca4aed
santi-git <santi.figueiras95@gmail.com>
2015-06-14 14:48:37 -0300

    Control sobre la oferta para distintos niveles de sesion

--- proyecto/function/ofertar-process.php
+++ proyecto/function/ofertar-process.php
@@ -11,20 +11,20 @@ if(isset($_POST)){
 		
 		require_once('../db/sanitize_input.php');
 
+		
 		$precio 	= 	sanitize($_POST['precio']);
-		$necesidad 	= 	ucfirst(sanitize($_POST['necesidad']);
+		$necesidad 	= 	sanitize($_POST['necesidad']);
 		$id_subasta = 	sanitize($_POST['id_subasta']);
 
-		/*"INSERT INTO usuario (id_usuario, dni, email, nombre_usuario, password, nombre, apellido, tarjeta, fecha_alta)
-						VALUES (NULL, '" . $_POST['dni'] ."', '" . $_POST['email'] . "' ,'" . $_POST['nombre_usuario'] . "', '" . $_POST['password'] . "', '" . $_POST['nombre'] . "', '" . $_POST['apellido'] ."', '" . $_POST['tarjeta'] . "', CURRENT_DATE())";
-		*/
+		$query = "	INSERT INTO oferta (id_oferta, id_usuario, id_subasta, necesidad, precio)
+					VALUES (NULL, '" . $_SESSION['id_usuario'] . "', '" . $id_subasta . "', '" . $necesidad . "', '" . $precio . "')";
 
-		$query = "	INSERT INTO oferta (id_oferta, , id_usuario, id_subasta, necesidad, precio)
-					VALUES (NULL, '" . $id_subasta . "', '" . $_SESSION['id_usuario'] . "', '" . $necesidad . "', '" . $precio . "')";
+		include('../db/connect.php');
 
-		include('../db/connect.php');		
-
-		header('Location: /index.php');
+		if($result = $db->query($query)){
+			header('Location: /index.php');
+		}					
+		
 	}
 }
 
--- proyecto/subasta.php
+++ proyecto/subasta.php
@@ -31,36 +31,42 @@ include('include/header.php');
 			<img class="responsive-img col m4" src="<?php echo $row->imagen_url ?>" alt="Imagen">
 			<div class="subasta-opciones">
 				<p><?php echo $row->descripcion ?></p>
-				<?php if($row->id_usuario == $_SESSION['id_usuario']){ ?>
+				<?php if($_SESSION){ ?>
 					
-					<a class="waves-effect waves-light btn">Ver Ofertas</a>
+					<?php if($row->id_usuario != $_SESSION['id_usuario']) {  ?>
+
+						<a class="waves-effect waves-light btn modal-trigger" href="#modal1">Ofertar</a>
+
+						<div id="modal1" class="modal">
+							<div class="modal-content">
+								<h4 class="center">Ofertar</h4>
+								<div class="row">
+									<form action="function/ofertar-process.php" method="post">
+										<div class="row">
+											<div class="input-field col s12">
+									          	<input name="precio" id="precio" type="number" class="validate" required>
+							          			<label for="precio">Precio</label>
+									        </div>
+									        <div class="input-field col s12">
+									          	<textarea name="necesidad" id="necesidad" class="materialize-textarea" required></textarea>
+	          									<label for="necesidad">Necesidad</label>
+									        </div>
+									        <input type="hidden" name="id_subasta" value="<?php echo $_GET['id_subasta'] ?>">
+							     		</div>	
+										<input id="subasta-submit" type="submit" class="btn right form-submit" value="Ofertar">
+										<a href="#!" class=" modal-action modal-close waves-effect waves-green btn red left">Cancelar</a>
+									</form>
+								</div>
+							</div>
+						</div>
+					<?php } else { ?>
+							<a class="waves-effect waves-light btn">Ver Ofertas</a>
+					<?php } ?>
 				
 				<?php } else { ?>
 					
-					<a class="waves-effect waves-light btn modal-trigger" href="#modal1">Ofertar</a>
+					<a class="waves-effect waves-light btn disabled">Ver Ofertas</a>
 
-					<div id="modal1" class="modal">
-						<div class="modal-content">
-							<h4 class="center">Ofertar</h4>
-							<div class="row">
-								<form action="function/ofertar-process.php" method="post">
-									<div class="row">
-										<div class="input-field col s12">
-								          	<input name="precio" id="precio" type="number" class="validate" required>
-						          			<label for="precio">Precio</label>
-								        </div>
-								        <div class="input-field col s12">
-								          	<textarea name="necesidad" id="necesidad" class="materialize-textarea" required></textarea>
-          									<label for="necesidad">Necesidad</label>
-								        </div>
-								        <input type="hidden" name="id_subasta" value="<?php echo $_GET['id_subasta'] ?>">
-						     		</div>	
-									<input id="subasta-submit" type="submit" class="btn right form-submit" value="Ofertar">
-									<a href="#!" class=" modal-action modal-close waves-effect waves-green btn red left">Cancelar</a>
-								</form>
-							</div>
-						</div>
-					</div>
 				<?php } ?>
 			</div>
 		</div>
