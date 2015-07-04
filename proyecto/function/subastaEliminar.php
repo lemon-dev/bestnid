6c53fc104040ab4f228ba41454323820ad1f5299
rnrpalacios <rnrpalacios@gmail.com>
2015-07-03 23:23:00 -0300

    arreglo eliminar subasta
    bajar base de datos desde drive, cambios cascade

--- proyecto/function/subastaEliminar.php
+++ proyecto/function/subastaEliminar.php
@@ -6,35 +6,51 @@
 	include('../db/connect.php');
 
 	if(isset($_POST)){
-		if(isset($_POST['idSubasta'])){
+		if(isset($_POST['idSubasta'])){	
+			
+							
+			//seleccionamos id de la oferta exitosa para borrarla (si hay)
+			$query="SELECT * FROM oferta_exitosa E INNER JOIN oferta O WHERE O.id_subasta = '".$_POST['idSubasta']."' AND E.id_oferta = O.id_oferta";
 
+			$result = $db->query($query);
+			$row=$result->fetch_object();
+			$id_oferta_exitosa=$row->id_oferta_exitosa;
+
+			echo $row->id_oferta_exitosa;
+
+			if($result->num_rows > 0){
+				$query="DELETE FROM oferta_exitosa WHERE id_oferta_exitosa = '".$id_oferta_exitosa."'";
+			}
+			
+			//borramos todas las ofertas que pertenecen a la subasta
+			$query="DELETE FROM oferta WHERE id_subasta ='".$_POST['idSubasta']."'";
+
+			$result = $db->query($query);
+			
+			//echo "elimino ofertas";
 
 			//seleccionamos id del producto para borrar el producto de la subasta
 			$query="SELECT * FROM subasta WHERE id_subasta ='".$_POST['idSubasta']."'";
 
 			$result = $db->query($query);
 			
-			while($row=$result->fetch_object()){
-				$idProducto=$row->id_producto;
-			}
+			$row=$result->fetch_object();
+			$idProducto=$row->id_producto;
 
-			//borramos todas las ofertas que pertenecen a la subasta
-			$query="DELETE FROM oferta WHERE id_subasta ='".$_POST['idSubasta']."'";
-
+			//borramos el producto que pertenece a la subasta
+			$query ="DELETE FROM producto WHERE id_producto ='".$idProducto."'";
+			
 			$result = $db->query($query);
 
 			//finalmente borramos la subasta 
 			$query =" DELETE FROM subasta WHERE id_subasta ='".$_POST['idSubasta']."'"; 
 
-			$result = $db->query($query);
-
-			//borramos el producto que pertenece a la subasta
-			$query ="DELETE FROM producto WHERE id_producto ='".$idProducto."'";
 			
 			if($result = $db->query($query)){
 				header('Location: /verPerfil.php');
-			}					
-			
+
+			}
+
 		}
 	}
 
