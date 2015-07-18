7cfa02ffce76aa2235cd0720d6a909297e18ceac
Juan Cruz Almazán <juancrujca@gmail.com>
2015-07-18 00:43:16 -0300

    Reporte usuario #1

--- proyecto/include/ver_usuarios.php
+++ proyecto/include/ver_usuarios.php
@@ -1,9 +1,20 @@
 <?php 
 
-$usuarios = usuarios();
-$total = 0;
+if(isset($_GET['desde'], $_GET['hasta'])):
+	//echo $_GET['desde'] . ' ' . $_GET['hasta'] . ' ';
+	include('../db/functions.php');
+	$usuarios = listar_usuarios_entre($_GET['desde'], $_GET['hasta']);
 
-if($usuarios->num_rows > 0): while($usuario = $usuarios->fetch_object()): 
+else: 
+	$usuarios = listar_usuarios();
+endif;
+
+$total = 0;
+?>
+
+<ul id="reporte_usuarios">
+
+<?php if($usuarios->num_rows > 0): while($usuario = $usuarios->fetch_object()): 
 	$total += 1;
 ?>
 	<li class="grey lighten-2 z-depth-1 card-panel ">
@@ -21,6 +32,17 @@ if($usuarios->num_rows > 0): while($usuario = $usuarios->fetch_object()):
 		</div>
 	</li>
 
-<?php endwhile; else: ?>
+<?php endwhile;?>
+
+</ul>
+
+<div class="card-panel <?php echo ($total > 0 ? 'green' : 'red') ?> z-depth-1 resumen">
+	<div class="row">
+		<p class="left">Total de usuarios:</p>
+		<p class="right"><?php echo $total ?></p>
+	</div>
+</div>
+
+<?php else: ?>
 	<h5>No hay resultados.</h5>
 <?php endif; ?>
